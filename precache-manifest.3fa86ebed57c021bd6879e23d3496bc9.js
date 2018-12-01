831a5a9baa355148a98dfef2254444ef34b9589b
xLsDg <xlsdg@qq.com>
2018-12-01 13:32:59 +0000

    Update demo

---
+++ precache-manifest.3fa86ebed57c021bd6879e23d3496bc9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "1babac4a98483fa8908f",
+    "url": "/static/js/main.1babac4a.chunk.js"
+  },
+  {
+    "revision": "b6d4ce495b3296103e74",
+    "url": "/static/js/1.b6d4ce49.chunk.js"
+  },
+  {
+    "revision": "1babac4a98483fa8908f",
+    "url": "/static/css/main.459a354d.chunk.css"
+  },
+  {
+    "revision": "2cca4525853e0ad10c4cf9722a629808",
+    "url": "/index.html"
+  }
+];
